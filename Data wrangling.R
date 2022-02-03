ccaf9002353a59979331cc216ba2331eabf0895f
sarahhagans <sarah.hagans13@gmail.com>
2022-02-03 12:09:40 -0600

    sarah's complete smoothness logit

--- Data wrangling.R
+++ Data wrangling.R
@@ -43,6 +43,7 @@ summary(lin_reg1)
 ###
 #Logistic regression Libraries
 library("caret")
+library("IDPmisc")
 library("magrittr")
 library("dplyr")
 library("tidyr")
@@ -51,19 +52,23 @@ library("popbio")
 library("e1071")
 
 #Begin Sarah's work#
-#Change name of column with spaces
 #I have the concave points variable as **concave.points_mean**
 
 #Sarah's Smoothness Basic Logistic Model
 summary(BC)
 BC$diagnosisR <- NA
-BC$diagnosisR[BC$diagnosis=='M'] <- 1
-BC$diagnosisR[BC$diagnosis=='B'] <- 0
-smoothlogit <- glm(diagnosis ~ smoothness_mean, data=BC, family="binomial")
+BC$diagnosisR[BC$diagnosis=='1'] <- 1
+BC$diagnosisR[BC$diagnosis=='0'] <- 0
+summary(BC)
+#Remove missing Variables
+BC$diagnosisR <- as.numeric(BC$diagnosisR)
+summary(BC)
+
+smoothlogit <- glm(diagnosisR ~ smoothness_mean, data=BC, family="binomial")
 #predict Diagnosis
 probabilitiesmooth <- predict(smoothlogit, type = "response")
 #Recode Predicted variable
-BC$Predicted <- ifelse(probabilities > .5, "pos", "neg")
+BC$Predicted <- ifelse(probabilitiesmooth > .5, "pos", "neg")
 BC$PredictedR <- NA
 BC$PredictedR[BC$Predicted =='pos'] <- 1
 BC$PredictedR[BC$Predicted =='neg'] <- 0
@@ -75,9 +80,48 @@ BC$diagnosisR <- as.factor(BC$diagnosisR)
 #Create Confusion Matrix
 conf_mat <- caret::confusionMatrix(BC$PredictedR, BC$diagnosisR)
 conf_mat
+#All cells were above 5 which is good.
+#Accuracy rating is 0.6766.
 
+#Logit Linearity
+#gather only numeric variables
+smooth1 <- BC %>%
+dplyr::select_if(is.numeric)
+
+#pull them to be fed into predictors
+predictors_smooth <- colnames(smooth1)
+#create final smooth logit
+smooth1 <- smooth1 %>%
+mutate(logit=log(probabilitiesmooth/(1-probabilitiesmooth))) %>%
+gather(key= "predictors", value = "predictor.value", -logit)
+#Graphing to assess linearity
+ggplot(smooth1, aes(logit, predictor.value))+
+  geom_point(size=.5, alpha=.5)+
+  geom_smooth(method= "loess")+
+  theme_bw()+
+  facet_wrap(~predictors, scales="free_y")
+#wow what a strong line smoothness_mean it is a negative slope.
+#graph residuals
+plot(smoothlogit$residuals)
+#pretty even distribution.
+
+#Screen for outliers
+infl <- influence.measures(smoothlogit)
+summary(infl)
+#dfb.1_ and dffitvalues are below 1 and hat values are below 0.3.
+#running logistic regression and interpreting out put
+summary(smoothlogit)
+#p- value is sig at0.001
+#Wald stat is -7.959
+#smoothness_mean estimate influence is -60.0857
+#AIC is 677.95
+
+#graphing it for a figure
+logi.hist.plot(BC$smoothness_mean,BC$diagnosisR, boxp=FALSE, type="hist", col="gray")
+#very flat don't use
+##
 #End Sarah's work#
-
+###
 
 
 
